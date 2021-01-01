7048288990d49b6c6bac9103dc7e3a9e4ad5fd29
wuu <415578597@qq.com>
2021-01-02 00:52:47 +1000

    Update-1
    all files

---
+++ JS/click.js
@@ -0,0 +1,11 @@
+function linkedin() {
+    console.log("linkedin");
+    window.open('http://www.linkedin.com/in/dongyu-wu97')
+
+}
+
+function git() {
+    console.log("github");
+    window.open('https://github.com/vincent-9-7') //新窗口打开
+    // window.location.href = 'https://github.com/vincent-9-7'; //相同页面打开
+}
